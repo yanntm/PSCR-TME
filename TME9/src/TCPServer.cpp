1fa44eed446bd207154edd2a221c9b1976c410fe
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2026-09-17 15:01:10 +0200

    add TCPServer skeleton

---
+++ TME9/src/TCPServer.cpp
@@ -0,0 +1,14 @@
+#include "TCPServer.h"
+
+namespace pr {
+
+bool TCPServer::startServer(int /* port */) {
+    // TODO : démarrer le serveur multi-thread (question 2).
+    return false;
+}
+
+void TCPServer::stopServer() {
+    // TODO : arrêter le serveur et terminer ses threads.
+}
+
+} // namespace pr
